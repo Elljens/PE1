cdaa1b704a33ca6e6bfe99a12bcc98a694cefa5d
Elljens <ellen.jensen81@gmail.com>
2026-02-19 10:06:45 +0100

    Bug fix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { BASE_URL, accessToken, name, isLoggedIn } from "./utils.js";
 
-const BlogPost_URL = `${BASE_URL}/blog/posts/${name}`;
+const BlogPost_URL = `${BASE_URL}/blog/posts/HenryDanger` && `${BASE_URL}/blog/posts/${name}`;
 const blogPostContainer = document.getElementById('blog-post-container');
 const carousel = document.getElementById('carousel');
 
